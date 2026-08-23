ac037d97c17dbc26b0e06e91af26aea0281a2885
alexvojproc <16513623+alexvojproc@users.noreply.github.com>
2026-08-23 22:56:29 +0100

    fix: drop script_dir

--- copr_script.sh
+++ copr_script.sh
@@ -9,9 +9,6 @@ set -euxo pipefail
 build_dir="$(realpath ..)"
 readonly build_dir
 
-script_dir="$(pwd)"
-readonly script_dir
-
 git clone https://src.fedoraproject.org/rpms/kernel.git
 cd kernel
 git checkout f44
@@ -233,6 +230,6 @@ sed --sandbox -i \
 # all end up in CONFIG_SYSTEM_TRUSTED_KEYS and thus .builtin_trusted_keys.
 cat "${build_dir}"/trusted-keys/*.pem > secureblue-certs.pem
 
-git apply "${script_dir}"/patches/*.patch
+git apply "${build_dir}"/patches/*.patch
 
 mv ./* "${build_dir}"
